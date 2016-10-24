05e96e858364ed1c7560b7dbd21a4f4cd1180a06
Evgenii Kuznetsov <Evgenii Kuznetsov>
2016-10-24 13:42:53 +0500

    Change deploy directory

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,6 +3,6 @@ import config from './config';
 import ghPages from 'gulp-gh-pages';
 
 export default function deploy() {
-	return gulp.src('**/*', {cwd: 'dest'})
+	return gulp.src('**/*', {cwd: config.destPath})
 		.pipe(ghPages());
 }
